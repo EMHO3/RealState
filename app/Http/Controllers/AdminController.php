a7725368113a0b85deb6abad5e9ebaec9b069423
Emir Huaroc <emirmho3@gmail.com>
2024-05-28 18:21:59 -0500

    funcion cambio de contraseña admin

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AdminController extends Controller
+{
+    public function AdminDashboard(){
+        return view('admin.index');
+    }
+    public function AdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    }// FIN DEL METODO
+
+    public function AdminLogin(){
+        return view('admin.admin_login');
+    }// FIN DEL METODO
+
+    public function AdminProfile(){
+        $id=Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view',compact('profileData'));
+    }// FIN DEL METODO
+
+    public function AdminProfileStore(Request $request){
+        $id=Auth::user()->id;
+        $data = User::find($id);
+        $data->username=$request->username;
+        $data->name=$request->name;
+        $data->email=$request->email;
+        $data->phone=$request->phone;
+        $data->address=$request->address;
+
+        if ($request->file('photo')) {
+            $file=$request->file('photo');
+            @unlink(public_path('upload/admin_images/'.$data->photo));
+            $filename=date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'),$filename);
+            $data['photo']=$filename;
+        }
+        $data->save();
+        $notificacion=array(
+            'message'=>'Admin Profile Updated Seccessfully',
+            'alert-type'=>'success'
+        );
+        return redirect()->back()->with($notificacion);
+
+    }//fin del metodo AdminProfileStore
+
+    public function AdminChangePassword(){
+        $id=Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_change_password',compact('profileData'));
+    }// FIN DEL METODO
+
+    public function AdminUpdatePassword (Request $request) {
+        //validacion
+        $request->validate([
+            'old_password'=>'required',
+            'new_password'=>'required|confirmed'
+        ]);
+
+        //coincide con la contraseña anterior
+        if (!Hash::check($request->old_password,auth::user()->password)) {
+            $notificacion=array(
+                'message'=>'Old Password Does not match',
+                'alert-type'=>'error'
+            );
+            return back()->with($notificacion);
+        }
+
+        //actualizacion de la nueva contraseña
+        User::whereId(auth()->user()->id)->update([
+            'password'=>Hash::make($request->new_password)
+        ]);
+        $notificacion=array(
+            'message'=>'Password change successfully',
+            'alert-type'=>'success'
+        );
+        return back()->with($notificacion);
+
+    }
+    // FIN DEL METODO
+}
